3e5b5b0ddbbd849a673ec14929b3a15a36817dfb
MostafaYahia <mostafayehyax23@gmail.com>
2020-06-30 10:53:44 +0200

    Add project files and dependencies

---
+++ src/main/java/com/mostafayahia/petclinicwebsite/PetClinicWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.mostafayahia.petclinicwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mostafayahia/petclinicwebsite/PetClinicWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mostafayahia.petclinicwebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
