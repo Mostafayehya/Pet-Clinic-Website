c5df65599ef973dc28fe6273e01e69085c7c46ff
MostafaYahia <mostafayehyax23@gmail.com>
2020-07-05 13:20:06 +0200

    Update maven-release version

--- pet-clinic-web/src/main/java/PetClinicWebsiteApplication.java
+++ pet-clinic-web/src/main/java/PetClinicWebsiteApplication.java
@@ -1,4 +1,5 @@
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
