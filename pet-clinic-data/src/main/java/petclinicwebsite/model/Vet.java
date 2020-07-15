a18f912d330a972c4a5a5118b9bb03f178e7dafd
MostafaYahia <mostafayehyax23@gmail.com>
2020-07-15 22:08:16 +0200

    Add Specialty, update Vet to include Specialty. Closes #28

--- pet-clinic-data/src/main/java/petclinicwebsite/model/Vet.java
+++ pet-clinic-data/src/main/java/petclinicwebsite/model/Vet.java
@@ -1,4 +1,16 @@
 package petclinicwebsite.model;
 
+import java.util.Set;
+
 public class Vet extends Person {
+
+    private Set<Specialty> specialtySet;
+
+    public Set<Specialty> getSpecialtySet() {
+        return specialtySet;
+    }
+
+    public void setSpecialtySet(Set<Specialty> specialtySet) {
+        this.specialtySet = specialtySet;
+    }
 }
